a8a4d583282cbe83af50a5a49fa5dce305914a9d
ellysaschottler <ellysastork@gmail.com>
2024-03-08 16:55:02 -0600

    created UserItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem'; 
 import './App.css';
 
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <h1>Hello from REact</h1>
+      <Navbar title= 'Github Finder' icon='fab fa-github' />
+      <UserItem />
     </div>
   );
 }
-  
+}
 export default App;
