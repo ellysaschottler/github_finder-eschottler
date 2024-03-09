bd4abea29f7aa7c3e96fa9df7b2f72c8d81f6cb4
ellysaschottler <ellysastork@gmail.com>
2024-03-09 14:39:14 -0600

    added spinner component and changed Users to functional component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,18 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react'
 import UserItem from './UserItem'
-
-class Users extends Component {
-    
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-            <UserItem key={user.id} user={user}/>
-            ))}
-      </div>
-    )
-  }
+import Spinner from '../layout/Spinner' 
+import PropTypes from 'prop-types'
+const Users = ({users, loading }) => {
+    if(loading) {
+        return <Spinner />
+    } else {
+        return (
+            <div style={userStyle}>
+              {users.map(user => (
+                  <UserItem key={user.id} user={user}/>
+                  ))}
+            </div>
+          );
+    }
 }
+
+Users.propTypes = {
+    users: PropTypes.array.isRequired,
+    loading: PropTypes.bool.isRequired
+}
+
 const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
