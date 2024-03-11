32ce57013580924997af0dbb9b6795a58668e280
ellysaschottler <ellysastork@gmail.com>
2024-03-11 09:11:03 -0500

    Created clear users button to show only after users have been displayed

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -6,7 +6,9 @@ export class Search extends Component {
         text: ''
     }
     static propTypes = {
-        searchUsers: PropTypes.func.isRequired
+        searchUsers: PropTypes.func.isRequired,
+        clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
     }
     onSubmit = e => {
         e.preventDefault();
@@ -17,12 +19,16 @@ export class Search extends Component {
     onChange = e => this.setState({ [e.target.name]: e.target.value });
     
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
             <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.onChange}/>
             <input type="submit" value="Search" className="btn btn-dark btn-block" />
         </form>
+        {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>Clear</button>
+        )}
       </div>
     )
   }
