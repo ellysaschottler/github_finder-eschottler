c8bff2ff460808e683676be54db9d44e13d7c25e
ellysaschottler <ellysastork@gmail.com>
2024-03-11 09:41:13 -0500

    created alert with 5 second timer if you try to search with a blank search field

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'; 
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
   // searches for first 30 users
   // async componentDidMount() {
@@ -30,13 +32,25 @@ class App extends Component {
   // Clear users from state
     clearUsers= () => this.setState({ users: [], loading: false});
 
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }});
+    setTimeout(() => this.setState({ alert: null}), 5000)
+  }
+
   render() {
     const { users, loading} = this.state;
   return (
     <div className="App">
       <Navbar title= 'Github Finder' icon='fab fa-github' />
       <div className='container'>
-        <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={users.length > 0 ? true: false}/>
+        <Alert alert={this.state.alert} />
+        <Search 
+          searchUsers={this.searchUsers} 
+          clearUsers={this.clearUsers} 
+          showClear={users.length > 0 ? true: false}
+          setAlert={this.setAlert}
+        />
         <Users loading={loading} users={users}/>
       </div>
     </div>
