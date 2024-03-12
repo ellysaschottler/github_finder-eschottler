b2e10d83d89f87bcd3ebf58b1cd79443058fa891
ellysaschottler <ellysastork@gmail.com>
2024-03-12 11:13:03 -0500

    took search user out of main app component and added it to context

--- src/App.js
+++ src/App.js
@@ -20,18 +20,6 @@ const App = () => {
   const [alert, setAlert] = useState(null);
 
 
-
-  // Search Github users
-  const searchUsers = async text => {
-    setLoading(true);
-
-    const res = await axios.get(
-      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-    setUsers(res.data.items);
-    setLoading(false)
-  }
-
-
   // Get single Gihub user
   const getUser = async (username) => {
     setLoading(true);
@@ -48,7 +36,7 @@ const App = () => {
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-      setRepos(res.data)
+      setRepos(res.data);
       setLoading(false);
   }
 
@@ -75,7 +63,6 @@ const App = () => {
           <Route exact path='/' render={props => (
             <Fragment>
               <Search 
-                searchUsers={searchUsers} 
                 clearUsers={clearUsers} 
                 showClear={users.length > 0 ? true: false}
                 setAlert={showAlert}
